07302650888c3ea60e1a5f8aa17f55c0d5e744dc
gocdevnhalam <gocdevnhalam.gmail.com>
2025-05-17 16:48:56 +0700

    feat: add revenue growth

--- app/manage_inventory/get_revenue/views.py
+++ app/manage_inventory/get_revenue/views.py
@@ -4,23 +4,65 @@ from .sql import *
 from django.db import connection
 from rest_framework.response import Response
 from django.utils.dateparse import parse_date
+from datetime import timedelta, date, datetime
 
 class GetRevenue(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
         try:
-            time_start = request.query_params.get('timeStart')
-            time_end = request.query_params.get('timeEnd')
+            time_type = request.query_params.get('timeType')
+            data_type = request.query_params.get('dataType')
+            now = datetime.now()
+
+            if time_type == '0':
+                today_start = datetime(now.year, now.month, now.day, 0,0,0,0)
+                today_end = datetime(now.year, now.month, now.day, 23,59,59,0)
+                yesterday = now - timedelta(days=1)
+                yesterday_start = datetime(yesterday.year, yesterday.month, yesterday.day, 0,0,0,0)
+                yesterday_end = datetime(yesterday.year, yesterday.month, yesterday.day, 23,59,59,59)
+
+                with connection.cursor() as cursor:
+                    if data_type == '0':
+                        cursor.execute(GET_REVENUE, [today_start, today_end])
+                        revenue_today = cursor.fetchone()[0] or 0
+
+                        cursor.execute(GET_REVENUE, [yesterday_start, yesterday_end])
+                        revenue_yesterday = cursor.fetchone()[0] or 0
+
+                return Response({
+                    "status": "1",
+                    "response": {
+                        "revenue": revenue_today,
+                        "revenue_before": revenue_yesterday
+                    }
+                })
+            elif time_type == '1':  # Tháng
+                first_day_this_month = datetime(now.year, now.month, 1, 0,0,0,0)
+                this_month_start = first_day_this_month
+                this_month_end = datetime(now.year, now.month, now.day, 23, 59, 59)
+
+                last_day_last_month = first_day_this_month - timedelta(days=1)
+                first_day_last_month = datetime(last_day_last_month.year, last_day_last_month.month, 1, 0,0,0,0)
+                last_month_start = first_day_last_month
+                last_month_end = datetime(last_day_last_month.year, last_day_last_month.month, last_day_last_month.day, 23, 59, 59, 59)
+
+                with connection.cursor() as cursor:
+                    if data_type == '0':
+                        cursor.execute(GET_REVENUE, [this_month_start, this_month_end])
+                        revenue_this_month = cursor.fetchone()[0] or 0
+
+                        cursor.execute(GET_REVENUE, [last_month_start, last_month_end])
+                        revenue_last_month = cursor.fetchone()[0] or 0
+
+                return Response({
+                    "status": "1",
+                    "response": {
+                        "revenue": revenue_this_month,
+                        "revenue_before": revenue_last_month
+                    }
+                })
         except Exception as error:
             return Response({
                 "status": "2",
-                "response": "error"
-            })
-
-        with connection.cursor() as cursor:
-            cursor.execute(GET_REVENUE, [time_start, time_end])
-            data = cursor.fetchone()[0]
-            return Response({
-                "status": "1",
-                "response": data
+                "response": error
             })
